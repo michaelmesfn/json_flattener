8a7171f1db5d39c3579a8513093024878db557cc
Michael Demse <michaelmesfn@gmail.com>
2021-03-31 23:00:07 +0200

    adding files

---
+++ json_flatten.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "json_flatten.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+//for nlohmann 3.9 and higher use:
+    //using json = nlohmann::ordered_json;
+int main()
+{
+    string input;
+    char buf;
+    while ((buf = getc(stdin)) != EOF)
+    {
+        input += buf;
+    }
+    json data = json::parse(input);
+    recurse <json> (data, "");
+    cout<<result.dump(4)<<endl;
+}
---
+++ json_flatten.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+//for nlohmann 3.9 and higher use:
+    //using json = nlohmann::ordered_json;
+using namespace std;
+
+json result;
+
+template <class T>
+void recurse(T cur, string prop)
+{
+    if (cur.type() != json::value_t::object)
+    {
+        result[prop] = cur;
+    }
+    else
+    {
+        bool isEmpty = true;
+        for (auto &p : cur.items())
+        {
+            isEmpty = false;
+            recurse <json> (cur[p.key()],  !prop.empty() ? prop + "." + p.key() : p.key());
+            if (isEmpty == true && !prop.empty()) {
+                result [prop] = {};
+            }
+        }
+    }
+}
+
+int sum (int a, int b) {
+    return a+b;
+}
